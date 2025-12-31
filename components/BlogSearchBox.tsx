84b26a1f899665087a08fb185264d35828876a3b
Habeebamoo <habeebamoo08@gmail.com>
2025-12-31 17:35:29 +0100

    created searchbox in blog page

--- components/BlogSearchBox.tsx
+++ components/BlogSearchBox.tsx
@@ -13,6 +13,46 @@ const BlogSearchBox = () => {
           placeholder="Search..." 
         />
       </div>
+
+      <div className="mt-8">
+        <h1 className="font-outfit text-[18px]">Recent Posts</h1>
+
+        {/* trending #1 */}
+        <div className="mt-6 flex justify-start items-start gap-4">
+          <div className="h-18 w-20 flex-none lg:h-20 lg:w-22">
+            <img src="/robot-dogs.jpg" className="h-full w-full" />
+          </div>
+          <div>
+            <div className="flex-start gap-2 text-[12px] flex-wrap">
+              <p className="font-jsans">TRENDING</p>
+              <div className="bg-gray-400 h-1 w-1 rounded-full"></div>
+              <span className="font-jsl text-gray-500">December 13, 2025</span>
+            </div>
+
+            <h1 className="font-outfit text-lg mt-2">The Evolution of Robotics is here in Nigeria, Africa</h1>
+          </div>
+        </div>
+
+        {/* trending #2 */}
+        <div className="mt-6 flex justify-start items-start gap-4">
+          <div className="h-18 w-20 flex-none lg:h-20 lg:w-22">
+            <img src="/robot-dogs.jpg" className="h-full w-full" />
+          </div>
+          <div>
+            <div className="flex-start gap-2 text-[12px] flex-wrap">
+              <p className="font-jsans">TRENDING</p>
+              <div className="bg-gray-400 h-1 w-1 rounded-full"></div>
+              <span className="font-jsl text-gray-500">December 13, 2025</span>
+            </div>
+
+            <h1 className="font-outfit text-lg mt-2">The Evolution of Robotics is here in Nigeria, Africa</h1>
+          </div>
+        </div>
+
+        <div className="h-15 mt-8 bg-gray-100">
+          
+        </div>
+      </div>
     </div>
   )
 }
