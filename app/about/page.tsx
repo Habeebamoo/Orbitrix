e0e8891c41b01fde277414d2b4b8e643cc8faa07
Habeebamoo <habeebamoo08@gmail.com>
2025-12-31 14:08:21 +0100

    created 3rd section in about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Header from "@/components/Header"
-import { ChevronDown } from "lucide-react"
+import { ArrowRight, ChevronDown } from "lucide-react"
 
 const AboutPage = () => {
   return (
@@ -51,13 +51,63 @@ const AboutPage = () => {
 
       </section>
 
+
       {/* 2nd Section */}
-      <section className="bg-gray-50 pt-10 sm:pt-20 pb-100">
+      <section className="bg-gray-50 pt-10 sm:pt-20">
         <p className="font-jsl text-center text-[14px] font-bold">WHAT WE OFFER</p>
 
         <h1 className="mt-2 md:mt-4 lg:mt-6 text-center font-jsans w-[85%] sm:w-[90%] mx-auto text-2xl sm:text-3xl md:text-4xl lg:font-bold xl:text-5xl">
-          Adventure beyond the stars, adrenaline that defies gravity – welcome to our cosmic
+          Adventure beyond the stars, adrenaline that defies gravity – welcome to our cosmic journey
         </h1>
+      </section>
+
+
+      {/* 3rd Section */}
+      <section className="bg-gray-50 pt-10 sm:pt-20 pb-100">
+        <p className="font-jsans text-center">Peter Bowman</p>
+        <p className="font-jsl text-gray-500 text-center text-sm">Creative Director</p>
+
+
+        {/* grid */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-6 px-6 md:px-10 xl:px-25">
+
+          <div className="px-6 py-10 bg-white border-1 border-stone-300">
+            <img src="/astro-icon.png" className="h-15 lg:h-20 mx-auto" />
+
+            <h1 className="font-jsans text-xl lg:text-2xl text-center mt-6">Space Travel</h1>
+
+            <p className="mt-2 text-[15px] font-jsl text-center text-gray-500">
+              Consectetur adipiscing elit, sed do eiusm od tempor incididunt ut labore.
+            </p>
+
+            <ArrowRight className="mt-4 mx-auto" />
+          </div>
+
+          <div className="px-6 py-10 bg-white border-1 border-stone-300">
+            <img src="/balloon-icon.png" className="h-15 lg:h-20 mx-auto" />
+
+            <h1 className="font-jsans text-xl lg:text-2xl text-center mt-6">Hot Air Ballons</h1>
+
+            <p className="mt-2 text-[15px] font-jsl text-center text-gray-500">
+              Consectetur adipiscing elit, sed do eiusm od tempor incididunt ut labore.
+            </p>
+
+            <ArrowRight className="mt-4 mx-auto" />
+          </div>
+
+          <div className="px-6 py-10 bg-white border-1 border-stone-300">
+            <img src="/jet-icon.png" className="h-25 lg:h-30 mx-auto" />
+
+            <h1 className="font-jsans text-xl lg:text-2xl text-center mt-4">Private Jets</h1>
+
+            <p className="mt-2 text-[15px] font-jsl text-center text-gray-500">
+              Consectetur adipiscing elit, sed do eiusm od tempor incididunt ut labore.
+            </p>
+
+            <ArrowRight className="mt-4 mx-auto" />
+          </div>
+          
+        </div>
       </section>
     </>
   )
